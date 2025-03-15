67be096090544459eeafd388f4e4f12a392417fd
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-03-15 14:27:16 +0600

    time out request of client to server or DB server

--- expressBasicServer/index.js
+++ expressBasicServer/index.js
@@ -1,22 +1,26 @@
 
 var express = require('express');
- var cookieParser = require('cookie-parser');
+ var timeout = require('connect-timeout');
  var app = express()
-    .use(cookieParser())
-    .use(function (req, res) {
-        if (req.cookies.name) {
-            console.log('User name:', req.cookies.name);
-        }
-        else {
-        res.cookie('name', 'foo');
-        }
-        res.end('Hello!');
-    })
-    .listen(30014);
+    .use('/api', timeout(5000),
+                function (req, res, next) {
+ // simulate a hanging request by doing nothing
+                }
+                , function (error, req, res, next) {
+                  if (req.timedout) {
+                      res.statusCode = 500;
+                      res.end('Request timed out');
+                  }
+                  else {
+                      next(error);
+                  }
+              })
+   
+    .listen(3014);
 
 /*
-    A cookie is some data sent from the web server and stored in the user’s web browser. Every time the user’s browser makes a request to the web server, the web browser will send back the cookie that it received from the server. Cookiesprovide a great foundation for creating user sessions.
-    The Express response object contains a few useful member functions to set client cookies. To set a cookie, call res.cookie(cookieName,value,[options]) function. For example, the code in Listing 7-9 will set a cookie called 'name' to 'foo':
-
-    If this response was handled by the browser, then that browser would always send the cookie called ‘name’ with value ‘foo’ if the path on the server starts with ‘/’ . The cookie is sent in the ‘cookie’ header by the client. In Listing 7-11, modify our server to log any cookies sent in the client request.
-    While the header is useful, you need something to parse this into a JavaScript object. That’s where the cookieparser (npm install cookie-parser) middleware comes in. Put this middleware in your queue and it populates the parsed cookies into the ‘req.cookies’ object, as shown in Listing 7-12, to demonstrate its usage. */
+    Time-out Hanging Requests
+ It is possible to get in a situation where some middleware fails to end the request and fails to call next. For example, this can happen if your database server is down and your middleware is waiting for a response from the database server. In such a case, the client HTTP request will be left hanging, taking up server memory. In these circumstances, you should time-out the client request instead of leaving it hanging.This is exactly what the connect-timeout (npm install connect-timeout) middleware is for */
+/*
+ If you start this webserver and visit http://localhost:3000/api, the request will hang for five seconds, after which the connect-timeout middleware kicks in and terminates the request, sending the client a 503 Service Unavailable HTTP response.
+ You can customize the response of a timeout by adding an error handling middleware and checking if a timeout occurred by checking the req.timedout property. */
