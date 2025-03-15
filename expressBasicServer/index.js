0200779489e9d2837c971dfd9ae946bb8b5b3c94
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-03-15 12:44:32 +0600

    express server to serve static content from public folder

--- expressBasicServer/index.js
+++ expressBasicServer/index.js
@@ -1,14 +1,11 @@
 const http = require('http');
 const express = require('express');
+const serveStatic= require('serve-static');
 
 // create an express app
 const app = express()
 // register a middleware
-            .use ((req, res, next) => {
-                console.log('middleware');
-                res.send('Hello World');
-               });
-
+            .use (serveStatic(__dirname + '/public'))
                // Register the app with the http server
 http.createServer(app).listen(3014, () => {
     console.log('Server started at http://localhost:3014');
