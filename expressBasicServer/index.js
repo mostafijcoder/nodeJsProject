ecbecd8a36bae459b8b7c1f270dae0b79a441b46
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-03-15 13:15:42 +0600

    client json request parsed and response text message

--- expressBasicServer/index.js
+++ expressBasicServer/index.js
@@ -1,13 +1,27 @@
-const http = require('http');
-const express = require('express');
-const serveStatic= require('serve-static');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express()
+   .use(express.json())
+   .use(express.urlencoded({ extended: true }))
+   .use(function (req, res) {
+       if (req.body.foo) {
+           res.end('Body parsed! Value of foo: ' + req.body.foo);
+       }
+       else {
+           res.end('Body does not have foo!');
+       }
+   })
+   .use(function (err, req, res, next) {
+       res.end('Invalid body!');
+   })
+   .listen(3014);
 
-// create an express app
-const app = express()
-// register a middleware
-            .use (serveStatic(__dirname + '/public'))
-               // Register the app with the http server
-http.createServer(app).listen(3014, () => {
-    console.log('Server started at http://localhost:3014');
-}
-);
+
+   /*Body parsing is the act of parsing a string based client request body into a JavaScript object that you can easily consume in your application code. This is a very common task in web development, making the body-parser middleware (npm install body-parser) a must-have in your tool-belt. It simply does the following two things:
+	
+   Parses the request body into a JavaScript object if the 
+  
+   Puts this JavaScript object (if the parse was successful) in 
+   content-type matches JSON 
+   (application/JSON) or a user submitted HTML form (the browser sends it as the MIME type 
+   application/x-www-form-urlencoded) middleware. */
