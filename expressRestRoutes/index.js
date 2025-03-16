cdde1c08b1cf69973f197d189b1094570e1fd5c7
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-03-16 11:34:50 +0600

    parameter based info loading with routing

--- expressRestRoutes/index.js
+++ expressRestRoutes/index.js
@@ -2,21 +2,22 @@
 var express = require('express');
 var app = express();
 
-app.get('/user/:userId', function(req, res) {
-    res.send('userId is set to ' + req.params['userId']);
+app.param('id', function(req, res, next, id) {
+    console.log('CALLED ONLY ONCE with', id);
+    res.write('Request URL: ' + req.originalUrl + '\n');
+    res.write('ID: ' + id + '\n');
+    req.user={id:id,name:'user'+  id};
+    next();
 });
-app.get('/user/:userId/role/:roleId', function(req, res) {
-    res.send('userId is set to ' + req.params['userId'] + ' and roleId is set to ' + req.params['roleId']);
-});
-app.get('/flights/:from-:to', function(req, res) {
-    res.send('from is set to ' + req.params['from'] + ' and to is set to ' + req.params['to']);
+app.get('/user/:id', function(req, res) {
+    res.write('Request URL: ' + req.originalUrl + '\n');
+    res.write('ID: ' + req.params.id + '\n');
+    res.write('User: ' + req.user.name + '\n');
+    res.end();
 });
 
 app.listen(3014, function() {
     console.log('Listening on port 3014');
 });
 /*
-Instead of putting too much filtering logic into your path prefix matching, a much nicer option is to use path 
-parameters. You can specify path parameters using the :parameterName syntax. For example `/user/:userId` will 
-match `/user/123` and populate the userId request parameter for you. Express puts all the parameter values in  
-req.params object */
+In fact, by using the app.param function, you can register a middleware to load the relevant information for you. The app.param middleware function is called whenever a parameter name matches in a route and is also passed in the parameter value as a fourth argument. */
