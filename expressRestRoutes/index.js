8734c63c1da817333a52076d67ac5f5dbf357bd8
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-03-16 10:44:35 +0600

    path prefix

--- expressRestRoutes/index.js
+++ expressRestRoutes/index.js
@@ -2,42 +2,18 @@
 var express = require('express');
 var app = express();
 
-app.route ('/')
-    .all(function(req, res, next) {
-        res.send('all\n');
-        next();
-    })
-    .get(function(req, res, next) {
-        res.send('get\n');
-        next();
-    })
-    .post(function(req, res, next) {
-        res.send('post\n');
-        next();
-    })
-    .put(function(req, res, next) {
-        res.send('put\n');
-        next();
-    })
-    .delete(function(req, res, next) {
-        res.send('delete\n');
-        next();
-    });
-app.listen(3014);
-
+app.get('/', function(req, res) {
+    res.send('nothing to see here');
+});
+app.get('/^\/[0-9]+$/', function(req, res) {
+    res.send('this is a zip code');
+});
+app.get('/*', function(req, res) {
+    res.send('this is a wildcard');
+});
+app.listen(3014, function() {
+    console.log('Listening on port 3014');
+});
 /*
-  app.route member function to specify the prefix only once, which returns a route object that 
-has the same all/get/put/post/delete functions
-
- $ curl http://127.0.0.1:3000
- all
- get
- $ curl -X PUT http://127.0.0.1:3000
- all
- put
- $ curl -X POST http://127.0.0.1:3000
- all
- post
- $ curl -X DELETE http://127.0.0.1:3000
- all
- delete */
+If you want to match a path prefix you can use the * 
+placeholder to match anything after the prefix. You can also set up a route based on a regular expression */
