709e7127033428f707221615fb882ba111f4c203
mostafijcoder <mostafijurrahmancoder@gmail.com>
2025-03-16 10:53:18 +0600

    parameter based routing

--- expressRestRoutes/index.js
+++ expressRestRoutes/index.js
@@ -2,18 +2,21 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('nothing to see here');
+app.get('/user/:userId', function(req, res) {
+    res.send('userId is set to ' + req.params['userId']);
 });
-app.get('/^\/[0-9]+$/', function(req, res) {
-    res.send('this is a zip code');
+app.get('/user/:userId/role/:roleId', function(req, res) {
+    res.send('userId is set to ' + req.params['userId'] + ' and roleId is set to ' + req.params['roleId']);
 });
-app.get('/*', function(req, res) {
-    res.send('this is a wildcard');
+app.get('/flights/:from-:to', function(req, res) {
+    res.send('from is set to ' + req.params['from'] + ' and to is set to ' + req.params['to']);
 });
+
 app.listen(3014, function() {
     console.log('Listening on port 3014');
 });
 /*
-If you want to match a path prefix you can use the * 
-placeholder to match anything after the prefix. You can also set up a route based on a regular expression */
+Instead of putting too much filtering logic into your path prefix matching, a much nicer option is to use path 
+parameters. You can specify path parameters using the :parameterName syntax. For example `/user/:userId` will 
+match `/user/123` and populate the userId request parameter for you. Express puts all the parameter values in  
+req.params object */
